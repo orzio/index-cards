5b9ec7faf3f6c6e65cd1d22be0988e72af83d85e
orzio <andrzej.orzinski@gmail.com>
2021-11-29 21:34:15 +0100

    add new dto

---
+++ Quiz.Core/DTO/CategoryWithSubcategoriesContainingQuestionsAndAnswers.cs
@@ -0,0 +1,15 @@
+﻿using Quiz.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Core.DTO
+{
+    public class CategoryWithSubcategoriesContainingQuestionsAndAnswers
+    {
+        public Category Category { get; set; }
+        public List<CategoryWithSubcategoriesContainingQuestionsAndAnswers> SubCategories { get; set; } = new();
+    }
+}
